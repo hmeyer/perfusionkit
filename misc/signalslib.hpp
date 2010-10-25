bb1da1731ba6cde2bf93d46e31a35c019d4b7824
Henning Meyer <Henning.Meyer@uhn.on.ca>
2010-10-25 12:31:21 -0400

    added copyright and gpl disclaimers

--- misc/signalslib.hpp
+++ misc/signalslib.hpp
@@ -1,3 +1,20 @@
+/*
+    This file is part of perfusionkit.
+
+    perfusionkit is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    perfusionkit is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with perfusionkit.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #ifndef SIGNALSLIB_HPP_INCLUDED
 #define SIGNALSLIB_HPP_INCLUDED 
 
